8b6f17702336dfb7667a3fce12702a420078aadf
ocliamd <ocliam@me.com>
2022-03-13 17:17:30 -0700

    first atempt at encryption

--- server/src/commands/core/chat.js
+++ server/src/commands/core/chat.js
@@ -21,6 +21,35 @@ const parseText = (text) => {
   return sanitizedText;
 };
 
+//encryption begins here
+function encrypt(text, s)
+    {
+        let result="";
+        for (let i = 0; i < text.length; i++)
+        {
+            let char = text[i];
+            if (char.toUpperCase(text[i]))
+            {
+                let ch =  String.fromCharCode((char.charCodeAt(0) + s-65) % 26 + 65);
+                result += ch;
+            }
+            else
+            {
+                let ch = String.fromCharCode((char.charCodeAt(0) + s-97) % 26 + 97);
+                result += ch;
+            }
+        }
+        return result;
+    }
+
+function decrypt(text, s)
+    {
+        let result = "";
+        s = (26-s) % 26;
+        result = encrypt(text, s);
+        return result; 
+    }
+
 // module main
 export async function run(core, server, socket, data) {
   // check user input
